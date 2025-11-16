b50ff62c073868950038e22e7c3ae5ce93d633f0
leezhengjing <zhengjing.lee86@gmail.com>
2025-11-16 19:05:19 +0800

    Initial Commit

---
+++ BFlix/BFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  BFlixApp.swift
+//  BFlix
+//
+//  Created by Lee Zheng Jing on 16/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
